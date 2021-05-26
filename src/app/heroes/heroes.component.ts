c39af358fe247b4389157c2c55f46f69c32f92cf
alex24mtc <alex25mtc@gmail.com>
2021-05-26 10:57:47 +0200

    10:57

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,28 +10,24 @@ import { HEROES } from '../../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-
-  };
-
-  selectedHero?: Hero;    // por defecto no esta seleccionado
-  onSelect(hero: Hero): void { 
-    this.selectedHero = hero; // cuando clicas se 
-  }
-
 
   heroes = HEROES;  //es la matriz de objetos de mock-heroes
+  selectedHero?: Hero;    // por defecto no esta seleccionado el heroe
 
 
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+    // cuando se selecciona un heroe
 
+    onSelect(hero: Hero): void { 
+      this.selectedHero = hero; // cuando clicas se actualiza la información automaticamente
+    }
+  
 
 
 }
